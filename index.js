485aee220932230de9fa5aae73acc03cbaf50fa6
Michael Bernasol <mfbernasol@gmail.com>
2020-11-09 11:37:59 -0600

    add fetch for load index

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 document.getElementById('load_console').addEventListener('click',displayIndex);
+document.getElementById('load_index').addEventListener('click',display);
+
+function display(){
+  fetch('https://mustang-index.azurewebsites.net/index.json')
+  .then(response => response.json())
+  .then(function(data){
+    data = JSON.stringify(data);
+    document.getElementById('indexID').innerHTML = data;
+  });
+}
 
 function displayIndex() {
   fetch('https://mustang-index.azurewebsites.net/index.json')
