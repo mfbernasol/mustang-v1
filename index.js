2461379d0af9fa7852ed2edf66dfc1130d9476a1
mfbernasol <mfbernasol@gmail.com>
2020-11-11 15:34:17 -0600

    working on fetch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.getElementById('load_console').addEventListener('click',displayIndex);
 document.getElementById('load_index').addEventListener('click',display);
+document.getElementById('load_contacts').addEventListener('click',displayContact);
 
 function display(){
   fetch('https://mustang-index.azurewebsites.net/index.json')
@@ -7,6 +8,18 @@ function display(){
   .then(function(data){
     data = JSON.stringify(data);
     document.getElementById('indexID').innerHTML = data;
+  });
+}
+
+function displayContact(){
+  fetch('https://mustang-index.azurewebsites.net/index.json')
+  .then(response => response.json())
+  .then(function(data){
+    data.forEach(function(user){
+      const obj = JSON.stringify(user.ContactURL);
+      console.log(user.ContactURL);
+      document.getElementById('contactID').innerHTML = obj;
+    })
   });
 }
 
